4bec4658b905aba6e2ee0714e9f3fb3f4715a053
Shreyas <shreyassingh621@gmail.com>
2024-11-20 11:11:20 +0530

    Sentry Updated

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -9,6 +9,7 @@ Sentry.init({
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+  enabled: process.env.SENTRY_DISABLE !== 'true',
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: true,
